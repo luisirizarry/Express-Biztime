8b27fae36a4a911192e627978ea09a3ad0e7a317
Luis Irizarry <lirizarry6178@gmail.com>
2024-11-24 22:51:28 -0500

    start of exercise

---
+++ db.js
@@ -0,0 +1,20 @@
+/** Database setup for BizTime. */
+
+const { Client } = require("pg");
+require("dotenv").config(); // Load environment variables from .env
+
+// Determine the database URI based on the environment
+let DB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.DB_URI_TEST
+    : process.env.DB_URI;
+
+// Set up the database client
+let db = new Client({
+  connectionString: DB_URI,
+});
+
+db.connect();
+
+module.exports = db;
+
